fdabddd60bbfc62b07b9b86f16103687dec62c98
m176 <m17683857924@163.com>
2020-07-15 15:47:00 +0800

    !!!

---
+++ src/main/java/com/example/medicine/bean/Hospital_Transaction_Details.java
@@ -0,0 +1,245 @@
+package com.example.medicine.bean;
+
+import java.util.Date;
+
+public class Hospital_Transaction_Details {
+    private Integer hospitalTransactionDetailsId;
+
+    private Integer purchaseOrderId;
+
+    private Integer drugInformationId;
+
+    private Integer supplierId;
+
+    private Double bidPrice;
+
+    private Double transactionPrice;
+
+    private String purchaseVolume;
+
+    private String purchasingStatus;
+
+    private String storageVolume;
+
+    private String purchaseAmount;
+
+    private Integer returnOrderId;
+
+    private String returnOrderNumber;
+
+    private String returnGoods;
+
+    private String warehousingAmount;
+
+    private String invoiceNumber;
+
+    private String drugBatchNumber;
+
+    private Date validityPeriodOfDrug;
+
+    private Date warehousing;
+
+    private String reasonForReturn;
+
+    private String returnStatus;
+
+    private Integer statementOfAccountId;
+
+    private String settlementVolume;
+
+    private String settlementAmount;
+
+    private String settlementStatus;
+
+    public Integer getHospitalTransactionDetailsId() {
+        return hospitalTransactionDetailsId;
+    }
+
+    public void setHospitalTransactionDetailsId(Integer hospitalTransactionDetailsId) {
+        this.hospitalTransactionDetailsId = hospitalTransactionDetailsId;
+    }
+
+    public Integer getPurchaseOrderId() {
+        return purchaseOrderId;
+    }
+
+    public void setPurchaseOrderId(Integer purchaseOrderId) {
+        this.purchaseOrderId = purchaseOrderId;
+    }
+
+    public Integer getDrugInformationId() {
+        return drugInformationId;
+    }
+
+    public void setDrugInformationId(Integer drugInformationId) {
+        this.drugInformationId = drugInformationId;
+    }
+
+    public Integer getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(Integer supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public Double getBidPrice() {
+        return bidPrice;
+    }
+
+    public void setBidPrice(Double bidPrice) {
+        this.bidPrice = bidPrice;
+    }
+
+    public Double getTransactionPrice() {
+        return transactionPrice;
+    }
+
+    public void setTransactionPrice(Double transactionPrice) {
+        this.transactionPrice = transactionPrice;
+    }
+
+    public String getPurchaseVolume() {
+        return purchaseVolume;
+    }
+
+    public void setPurchaseVolume(String purchaseVolume) {
+        this.purchaseVolume = purchaseVolume == null ? null : purchaseVolume.trim();
+    }
+
+    public String getPurchasingStatus() {
+        return purchasingStatus;
+    }
+
+    public void setPurchasingStatus(String purchasingStatus) {
+        this.purchasingStatus = purchasingStatus == null ? null : purchasingStatus.trim();
+    }
+
+    public String getStorageVolume() {
+        return storageVolume;
+    }
+
+    public void setStorageVolume(String storageVolume) {
+        this.storageVolume = storageVolume == null ? null : storageVolume.trim();
+    }
+
+    public String getPurchaseAmount() {
+        return purchaseAmount;
+    }
+
+    public void setPurchaseAmount(String purchaseAmount) {
+        this.purchaseAmount = purchaseAmount == null ? null : purchaseAmount.trim();
+    }
+
+    public Integer getReturnOrderId() {
+        return returnOrderId;
+    }
+
+    public void setReturnOrderId(Integer returnOrderId) {
+        this.returnOrderId = returnOrderId;
+    }
+
+    public String getReturnOrderNumber() {
+        return returnOrderNumber;
+    }
+
+    public void setReturnOrderNumber(String returnOrderNumber) {
+        this.returnOrderNumber = returnOrderNumber == null ? null : returnOrderNumber.trim();
+    }
+
+    public String getReturnGoods() {
+        return returnGoods;
+    }
+
+    public void setReturnGoods(String returnGoods) {
+        this.returnGoods = returnGoods == null ? null : returnGoods.trim();
+    }
+
+    public String getWarehousingAmount() {
+        return warehousingAmount;
+    }
+
+    public void setWarehousingAmount(String warehousingAmount) {
+        this.warehousingAmount = warehousingAmount == null ? null : warehousingAmount.trim();
+    }
+
+    public String getInvoiceNumber() {
+        return invoiceNumber;
+    }
+
+    public void setInvoiceNumber(String invoiceNumber) {
+        this.invoiceNumber = invoiceNumber == null ? null : invoiceNumber.trim();
+    }
+
+    public String getDrugBatchNumber() {
+        return drugBatchNumber;
+    }
+
+    public void setDrugBatchNumber(String drugBatchNumber) {
+        this.drugBatchNumber = drugBatchNumber == null ? null : drugBatchNumber.trim();
+    }
+
+    public Date getValidityPeriodOfDrug() {
+        return validityPeriodOfDrug;
+    }
+
+    public void setValidityPeriodOfDrug(Date validityPeriodOfDrug) {
+        this.validityPeriodOfDrug = validityPeriodOfDrug;
+    }
+
+    public Date getWarehousing() {
+        return warehousing;
+    }
+
+    public void setWarehousing(Date warehousing) {
+        this.warehousing = warehousing;
+    }
+
+    public String getReasonForReturn() {
+        return reasonForReturn;
+    }
+
+    public void setReasonForReturn(String reasonForReturn) {
+        this.reasonForReturn = reasonForReturn == null ? null : reasonForReturn.trim();
+    }
+
+    public String getReturnStatus() {
+        return returnStatus;
+    }
+
+    public void setReturnStatus(String returnStatus) {
+        this.returnStatus = returnStatus == null ? null : returnStatus.trim();
+    }
+
+    public Integer getStatementOfAccountId() {
+        return statementOfAccountId;
+    }
+
+    public void setStatementOfAccountId(Integer statementOfAccountId) {
+        this.statementOfAccountId = statementOfAccountId;
+    }
+
+    public String getSettlementVolume() {
+        return settlementVolume;
+    }
+
+    public void setSettlementVolume(String settlementVolume) {
+        this.settlementVolume = settlementVolume == null ? null : settlementVolume.trim();
+    }
+
+    public String getSettlementAmount() {
+        return settlementAmount;
+    }
+
+    public void setSettlementAmount(String settlementAmount) {
+        this.settlementAmount = settlementAmount == null ? null : settlementAmount.trim();
+    }
+
+    public String getSettlementStatus() {
+        return settlementStatus;
+    }
+
+    public void setSettlementStatus(String settlementStatus) {
+        this.settlementStatus = settlementStatus == null ? null : settlementStatus.trim();
+    }
+}
---
+++ src/main/java/com/example/medicine/bean/Vendor_Information.java
@@ -0,0 +1,275 @@
+package com.example.medicine.bean;
+
+import java.util.Date;
+
+public class Vendor_Information {
+    private Integer supplierId;
+
+    private String nameOfEnterprise;
+
+    private Integer enterpriseCategory;
+
+    private String licence;
+
+    private Date licenseExpirationTime;
+
+    private String businessContact;
+
+    private String businessContactNumber;
+
+    private String businessScope;
+
+    private String registeredAddress;
+
+    private String contactAddress;
+
+    private String postalOde;
+
+    private Double totalAssets;
+
+    private String enterpriseFax;
+
+    private String nameOfLegalRepresentative;
+
+    private String corporateIdentityCard;
+
+    private String registeredCapitalOfAnEnterprise;
+
+    private String salesAmountOfLastYear;
+
+    private String email;
+
+    private String website;
+
+    private String enterpriseCodeCertificateNo;
+
+    private String validityPeriodOfEnterpriseCode;
+
+    private String enterpriseLetterOfCredit;
+
+    private String letterOfCreditNo;
+
+    private String validityOfLetterOfCredit;
+
+    private String fixedAssets;
+
+    private String companyProfile;
+
+    private String remarks;
+
+    public Integer getSupplierId() {
+        return supplierId;
+    }
+
+    public void setSupplierId(Integer supplierId) {
+        this.supplierId = supplierId;
+    }
+
+    public String getNameOfEnterprise() {
+        return nameOfEnterprise;
+    }
+
+    public void setNameOfEnterprise(String nameOfEnterprise) {
+        this.nameOfEnterprise = nameOfEnterprise == null ? null : nameOfEnterprise.trim();
+    }
+
+    public Integer getEnterpriseCategory() {
+        return enterpriseCategory;
+    }
+
+    public void setEnterpriseCategory(Integer enterpriseCategory) {
+        this.enterpriseCategory = enterpriseCategory;
+    }
+
+    public String getLicence() {
+        return licence;
+    }
+
+    public void setLicence(String licence) {
+        this.licence = licence == null ? null : licence.trim();
+    }
+
+    public Date getLicenseExpirationTime() {
+        return licenseExpirationTime;
+    }
+
+    public void setLicenseExpirationTime(Date licenseExpirationTime) {
+        this.licenseExpirationTime = licenseExpirationTime;
+    }
+
+    public String getBusinessContact() {
+        return businessContact;
+    }
+
+    public void setBusinessContact(String businessContact) {
+        this.businessContact = businessContact == null ? null : businessContact.trim();
+    }
+
+    public String getBusinessContactNumber() {
+        return businessContactNumber;
+    }
+
+    public void setBusinessContactNumber(String businessContactNumber) {
+        this.businessContactNumber = businessContactNumber == null ? null : businessContactNumber.trim();
+    }
+
+    public String getBusinessScope() {
+        return businessScope;
+    }
+
+    public void setBusinessScope(String businessScope) {
+        this.businessScope = businessScope == null ? null : businessScope.trim();
+    }
+
+    public String getRegisteredAddress() {
+        return registeredAddress;
+    }
+
+    public void setRegisteredAddress(String registeredAddress) {
+        this.registeredAddress = registeredAddress == null ? null : registeredAddress.trim();
+    }
+
+    public String getContactAddress() {
+        return contactAddress;
+    }
+
+    public void setContactAddress(String contactAddress) {
+        this.contactAddress = contactAddress == null ? null : contactAddress.trim();
+    }
+
+    public String getPostalOde() {
+        return postalOde;
+    }
+
+    public void setPostalOde(String postalOde) {
+        this.postalOde = postalOde == null ? null : postalOde.trim();
+    }
+
+    public Double getTotalAssets() {
+        return totalAssets;
+    }
+
+    public void setTotalAssets(Double totalAssets) {
+        this.totalAssets = totalAssets;
+    }
+
+    public String getEnterpriseFax() {
+        return enterpriseFax;
+    }
+
+    public void setEnterpriseFax(String enterpriseFax) {
+        this.enterpriseFax = enterpriseFax == null ? null : enterpriseFax.trim();
+    }
+
+    public String getNameOfLegalRepresentative() {
+        return nameOfLegalRepresentative;
+    }
+
+    public void setNameOfLegalRepresentative(String nameOfLegalRepresentative) {
+        this.nameOfLegalRepresentative = nameOfLegalRepresentative == null ? null : nameOfLegalRepresentative.trim();
+    }
+
+    public String getCorporateIdentityCard() {
+        return corporateIdentityCard;
+    }
+
+    public void setCorporateIdentityCard(String corporateIdentityCard) {
+        this.corporateIdentityCard = corporateIdentityCard == null ? null : corporateIdentityCard.trim();
+    }
+
+    public String getRegisteredCapitalOfAnEnterprise() {
+        return registeredCapitalOfAnEnterprise;
+    }
+
+    public void setRegisteredCapitalOfAnEnterprise(String registeredCapitalOfAnEnterprise) {
+        this.registeredCapitalOfAnEnterprise = registeredCapitalOfAnEnterprise == null ? null : registeredCapitalOfAnEnterprise.trim();
+    }
+
+    public String getSalesAmountOfLastYear() {
+        return salesAmountOfLastYear;
+    }
+
+    public void setSalesAmountOfLastYear(String salesAmountOfLastYear) {
+        this.salesAmountOfLastYear = salesAmountOfLastYear == null ? null : salesAmountOfLastYear.trim();
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email == null ? null : email.trim();
+    }
+
+    public String getWebsite() {
+        return website;
+    }
+
+    public void setWebsite(String website) {
+        this.website = website == null ? null : website.trim();
+    }
+
+    public String getEnterpriseCodeCertificateNo() {
+        return enterpriseCodeCertificateNo;
+    }
+
+    public void setEnterpriseCodeCertificateNo(String enterpriseCodeCertificateNo) {
+        this.enterpriseCodeCertificateNo = enterpriseCodeCertificateNo == null ? null : enterpriseCodeCertificateNo.trim();
+    }
+
+    public String getValidityPeriodOfEnterpriseCode() {
+        return validityPeriodOfEnterpriseCode;
+    }
+
+    public void setValidityPeriodOfEnterpriseCode(String validityPeriodOfEnterpriseCode) {
+        this.validityPeriodOfEnterpriseCode = validityPeriodOfEnterpriseCode == null ? null : validityPeriodOfEnterpriseCode.trim();
+    }
+
+    public String getEnterpriseLetterOfCredit() {
+        return enterpriseLetterOfCredit;
+    }
+
+    public void setEnterpriseLetterOfCredit(String enterpriseLetterOfCredit) {
+        this.enterpriseLetterOfCredit = enterpriseLetterOfCredit == null ? null : enterpriseLetterOfCredit.trim();
+    }
+
+    public String getLetterOfCreditNo() {
+        return letterOfCreditNo;
+    }
+
+    public void setLetterOfCreditNo(String letterOfCreditNo) {
+        this.letterOfCreditNo = letterOfCreditNo == null ? null : letterOfCreditNo.trim();
+    }
+
+    public String getValidityOfLetterOfCredit() {
+        return validityOfLetterOfCredit;
+    }
+
+    public void setValidityOfLetterOfCredit(String validityOfLetterOfCredit) {
+        this.validityOfLetterOfCredit = validityOfLetterOfCredit == null ? null : validityOfLetterOfCredit.trim();
+    }
+
+    public String getFixedAssets() {
+        return fixedAssets;
+    }
+
+    public void setFixedAssets(String fixedAssets) {
+        this.fixedAssets = fixedAssets == null ? null : fixedAssets.trim();
+    }
+
+    public String getCompanyProfile() {
+        return companyProfile;
+    }
+
+    public void setCompanyProfile(String companyProfile) {
+        this.companyProfile = companyProfile == null ? null : companyProfile.trim();
+    }
+
+    public String getRemarks() {
+        return remarks;
+    }
+
+    public void setRemarks(String remarks) {
+        this.remarks = remarks == null ? null : remarks.trim();
+    }
+}
